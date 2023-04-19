ee97f6f3dff03968af9ca6f3742ec84e878a5333
DavydVeremchuk <davyd.v@code-care.pro>
2023-04-19 06:35:34 +0300

    feat: implement weather tile fetching

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,65 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapboxGL from '@rnmapbox/maps';
-
+import { zalupaEbanaBlyat } from './zalupa-ebana-blyat';
 MapboxGL.setAccessToken(
   'pk.eyJ1IjoiMW5zcGlyM2QiLCJhIjoiY2xmdWdhbzhsMDJkODNqbXU0ZXduenB4eSJ9.3gjSoGxYCmFckQ9g_JBnaA',
 );
 MapboxGL.setWellKnownTileServer(MapboxGL.TileServers.Mapbox);
 
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-
-  map: {
-    flex: 1,
-  },
-});
-
-fetch(
-  'https://b.maps.owm.io/weather/cities/2/0/0?appid=b1b15e88fa797225412429c1c50c122a1',
-)
-  .then(() => 'success')
-  .catch(() => 'error');
+const getColor = (temp: number): string => {
+  if (temp <= 0) {
+    return '#1c71f2';
+  }
+  if (temp <= 10) {
+    return '#60bdfa';
+  }
+  if (temp <= 20) {
+    return '#e8d024';
+  }
+  if (temp <= 30) {
+    return '#fbaa1b';
+  }
+  return '#f56048';
+};
 
 const App = (): JSX.Element => {
+  const [urls, setUrls] = useState<string[] | null>(null);
+  const [shapes, setShapes] = useState<any>([]);
+  console.log('shapes: ', shapes.length);
   useEffect(() => {
     MapboxGL.setTelemetryEnabled(false);
   }, []);
 
+  useEffect(() => {
+    if (!urls) {
+      return;
+    }
+
+    Promise.all(
+      urls.map(async url => {
+        const data = await fetch(url, {
+          method: 'GET',
+        });
+        return await data.json().then(data => data.features);
+      }),
+    ).then(res => setShapes(res.flat(1)));
+  }, [urls]);
+
   return (
     <View style={styles.page}>
       <MapboxGL.MapView
+        rotateEnabled={false}
+        pitchEnabled={false}
         style={styles.map}
+        onRegionDidChange={feature => {
+          setUrls(
+            zalupaEbanaBlyat(
+              feature.properties.visibleBounds,
+              feature.properties.zoomLevel,
+            ),
+          );
+        }}
         compassEnabled={true}
         // styleURL={MapboxGL.StyleURL.Dark}
         styleJSON={JSON.stringify({
@@ -46,7 +73,7 @@ const App = (): JSX.Element => {
               ],
               tileSize: 256,
               minzoom: 1,
-              maxzoom: 19,
+              maxzoom: 10,
             },
           },
           layers: [
@@ -61,50 +88,53 @@ const App = (): JSX.Element => {
           ],
         })}>
         <MapboxGL.Camera
+          maxZoomLevel={10}
+          minZoomLevel={1}
           defaultSettings={{
             zoomLevel: 1,
-            centerCoordinate: [47.5361, 20.2456],
+            centerCoordinate: [0, 0],
             pitch: 0,
           }}
         />
-        <MapboxGL.Images
-          images={{
-            test: {
-              uri: 'https://openweathermap.org/img/wn/10d@4x.png',
-            },
-          }}
-        />
-        <MapboxGL.ShapeSource
-          shape={{
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [0, 0],
-            },
-            properties: {
-              city: 'Antananarivo',
-              clouds: 75,
-              country: 'MG',
-              humidity: 73,
-              pressure: 1021,
-              temp: 22.98,
-              feels_like: 23.24,
-              wind_deg: 90,
-              wind_speed: 3.6,
-            },
-          }}>
-          <MapboxGL.SymbolLayer
-            id="test-image"
-            aboveLayerID="cities"
-            style={{
-              iconImage: 'test',
-              iconSize: 1,
-            }}
-          />
-        </MapboxGL.ShapeSource>
+        {shapes.map((feature, id) => (
+          <MapboxGL.ShapeSource id={feature?.properties?.city} shape={feature}>
+            <MapboxGL.CircleLayer
+              id={`${feature?.properties?.city}-marker`}
+              aboveLayerID="background"
+              style={{
+                circleRadius: 20,
+                circleColor: getColor(feature.properties.temp),
+                circleStrokeColor: 'white',
+                circleStrokeWidth: 2,
+              }}>
+              <MapboxGL.SymbolLayer
+                id={`${feature?.properties?.city}-label`}
+                aboveLayerID="background"
+                style={{
+                  textField: 'ZALUPA',
+                  textColor: 'green',
+                  textSize: 12,
+                  textAllowOverlap: true,
+                  textIgnorePlacement: true,
+                }}
+              />
+            </MapboxGL.CircleLayer>
+          </MapboxGL.ShapeSource>
+        ))}
       </MapboxGL.MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+
+  map: {
+    flex: 1,
+  },
+});
 
 export default App;
---
+++ src/zalupa-ebana-blyat.ts
@@ -0,0 +1,55 @@
+import { Position } from '@rnmapbox/maps/lib/typescript/types/Position';
+
+const buildUrl = (z: number, x: number, y: number): string => {
+  return `https://b.maps.owm.io/weather/cities/${z}/${x}/${y}?appid=b1b15e88fa797225412429c1c50c122a1`;
+};
+
+export const zalupaEbanaBlyat = (
+  bounds: Position[],
+  zoom: number,
+): string[] => {
+  //   sinLatitude = sin(latitude * pi/180)
+  // pixelX = ((longitude + 180) / 360) * 256 * 2 level
+  // pixelY = (0.5 – log((1 + sinLatitude) / (1 – sinLatitude)) / (4 * pi)) * 256 * 2 level
+  const poweredToZoom = Math.pow(2, zoom);
+  const maxTileXY = poweredToZoom - 1;
+  const [ne, sw] = bounds;
+
+  const NELon = ne[0] > 180 ? ne[0] - 360 : ne[0];
+  const SWLon = sw[0] > 180 ? sw[0] - 360 : sw[0];
+
+  const sinLatNE = Math.sin(ne[1] * (Math.PI / 180));
+  const pixelXNE = ((NELon + 180) / 360) * 256 * poweredToZoom;
+  const pixelYNE =
+    (0.5 - Math.log((1 + sinLatNE) / (1 - sinLatNE)) / (4 * Math.PI)) *
+    256 *
+    poweredToZoom;
+
+  const sinLatSW = Math.sin(sw[1] * (Math.PI / 180));
+  const pixelXSW = ((SWLon + 180) / 360) * 256 * poweredToZoom;
+  const pixelYSW =
+    (0.5 - Math.log((1 + sinLatSW) / (1 - sinLatSW)) / (4 * Math.PI)) *
+    256 *
+    poweredToZoom;
+
+  const range = [
+    [
+      Math.min(Math.floor(pixelXNE / 256), maxTileXY),
+      Math.min(Math.floor(pixelYNE / 256), maxTileXY),
+    ],
+    [
+      Math.min(Math.floor(pixelXSW / 256), maxTileXY),
+      Math.min(Math.floor(pixelYSW / 256), maxTileXY),
+    ],
+  ];
+
+  const urls = [];
+
+  for (let x = range[1][0]; x <= range[0][0]; x++) {
+    for (let y = range[0][1]; y <= range[1][1]; y++) {
+      urls.push(buildUrl(zoom, x, y));
+    }
+  }
+  console.log(urls);
+  return urls;
+};
